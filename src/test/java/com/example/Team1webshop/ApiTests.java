56cd75ffeb9e01d7b189f18f3255d7ccfe426544
korsb <korsbacken@gmail.com>
2024-03-15 07:54:42 +0100

    Changes to pom and added test

---
+++ src/test/java/com/example/Team1webshop/ApiTests.java
@@ -0,0 +1,29 @@
+package com.example.Team1webshop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.hasItems;
+
+@SpringBootTest
+public class ApiTests {
+
+    // Samuel
+    @Test
+    @DisplayName("Check endpoint status code and if all categories are available")
+    public void webShopCategories() {
+        baseURI = "https://produktapi-6ef53ba8f2f2.herokuapp.com";
+        given().
+            get("/products/categories").
+        then().statusCode(200).body("", hasItems(
+            "electronics",
+            "jewelery",
+            "men's clothing",
+            "women's clothing"));
+    }
+
+
+
+}
