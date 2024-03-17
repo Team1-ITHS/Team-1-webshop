6a05f305215527d2dd22ac61387800841e6084da
korsb <korsbacken@gmail.com>
2024-03-17 19:42:59 +0100

    Added api tests for electronics category

--- src/test/java/com/example/Team1webshop/ApiTests.java
+++ src/test/java/com/example/Team1webshop/ApiTests.java
@@ -3,9 +3,8 @@ package com.example.Team1webshop;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 public class ApiTests {
@@ -24,6 +23,43 @@ public class ApiTests {
             "women's clothing"));
     }
 
+    // Samuel
+    @Test
+    @DisplayName("Check endpoint status code of electronics category and make sure it is not empty")
+    public void webShopElectronicsCategory() {
+        baseURI = "https://produktapi-6ef53ba8f2f2.herokuapp.com";
+        given().
+            get("/products/categories/electronics").
+        then().statusCode(200).body("$", hasSize(greaterThan(0)));
+    }
 
+    // Samuel
+    @Test
+    @DisplayName("Verify the title of a product in the electronics category")
+    public void webShopElectronicsCategoryProductDataTitle() {
+        baseURI = "https://produktapi-6ef53ba8f2f2.herokuapp.com";
+        given().
+            get("/products/categories/electronics").
+        then().statusCode(200).body("[0].title", equalTo("WD 2TB Elements Portable External Hard Drive - USB 3.0"));
+    }
 
+    // Samuel
+    @Test
+    @DisplayName("Verify the price of a product in the electronics category")
+    public void webShopElectronicsCategoryProductDataPrice() {
+        baseURI = "https://produktapi-6ef53ba8f2f2.herokuapp.com";
+        given().
+            get("/products/categories/electronics").
+        then().statusCode(200).body("[1].price", equalTo(109F));
+    }
+
+    // Samuel
+    @Test
+    @DisplayName("Verify the id of a product in the electronics category")
+    public void webShopElectronicsCategoryProductDataId() {
+        baseURI = "https://produktapi-6ef53ba8f2f2.herokuapp.com";
+        given().
+            get("/products/categories/electronics").
+        then().statusCode(200).body("[2].id", equalTo(11));
+    }
 }
