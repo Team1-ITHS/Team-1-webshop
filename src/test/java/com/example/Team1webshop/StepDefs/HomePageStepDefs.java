58e0ad01153f9dea968c4bba85ae177056dcecee
mhuggare <160525267+mhuggare@users.noreply.github.com>
2024-03-22 11:42:53 +0100

    added test on homepage

--- src/test/java/com/example/Team1webshop/StepDefs/HomePageStepDefs.java
+++ src/test/java/com/example/Team1webshop/StepDefs/HomePageStepDefs.java
@@ -4,6 +4,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -24,6 +25,30 @@ public class HomePageStepDefs {
     @Then("the page title should be Webbutiken")
     public void the_page_title_should_be_webbutiken() {
         Assertions.assertTrue(true);
+    }
+
+
+    @When("the user clicks on the {string} link") //Mia
+    public void the_user_clicks_on_the_link(String shopLink) {
+        driver.findElement(By.linkText(shopLink)).click();
+    }
+    @Then("the user should be redirected to {string}") //Mia
+    public void the_user_should_be_redirected_to(String expectedUrl) {
+        String actualUrl = driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl, actualUrl);
+    }
+
+    @When("the user click on the {string} link") //Mia
+    public void the_user_click_on_the_link(String aboutLink) {
+        //To be added when you can click about link
+        //driver.findElement(By.linkText(aboutLink)).click();
+    }
+
+    @Then("the users should be redirected to {string}") //Mia
+    public void the_users_should_be_redirected_to(String expected_Url) {
+        Assertions.assertTrue(true); //to be removed when actual About-link exists
+        //String actual_Url = driver.getCurrentUrl();
+        //Assertions.assertEquals(expected_Url, actual_Url, "Expected URL: " + expected_Url + ", Actual URL: " + actual_Url);
     }
 
     @After
--- src/test/java/com/example/Team1webshop/resources/features/homePage.feature
+++ src/test/java/com/example/Team1webshop/resources/features/homePage.feature
@@ -6,3 +6,15 @@ Feature: https://webshop-agil-testautomatiserare.netlify.app/
 
   Scenario: page title test
     Then the page title should be Webbutiken
+
+    #Mia
+  Scenario: Click on the Shop link
+    When the user clicks on the "Shop" link
+    Then the user should be redirected to "https://webshop-agil-testautomatiserare.netlify.app/products"
+
+    #Mia
+  Scenario: Click on the About link
+    When the user click on the "About" link
+    Then the users should be redirected to "https://webshop-agil-testautomatiserare.netlify.app/#"
+    #when About-site is available change to row below
+    #Then the users should be redirected to "https://webshop-agil-testautomatiserare.netlify.app/about"
