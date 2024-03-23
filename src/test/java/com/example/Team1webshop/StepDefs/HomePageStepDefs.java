b6d56aed9b047d90e9446c4903444178df59e21f
korsb <korsbacken@gmail.com>
2024-03-23 11:39:03 +0100

    Trying to get latest code

--- src/test/java/com/example/Team1webshop/StepDefs/HomePageStepDefs.java
+++ src/test/java/com/example/Team1webshop/StepDefs/HomePageStepDefs.java
@@ -1,34 +1,4 @@
 package com.example.Team1webshop.StepDefs;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.*;
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class HomePageStepDefs {
-
-    static WebDriver driver;
-
-    @Before
-    public void setup(){
-        driver = new ChromeDriver();
-    }
-
-    @Given("User has navigated to the start page")
-    public void user_has_navigated_to_the_start_page() {
-        driver.get("https://webshop-agil-testautomatiserare.netlify.app/");
-    }
-
-    @Then("the page title should be Webbutiken")
-    public void the_page_title_should_be_webbutiken() {
-        Assertions.assertTrue(true);
-    }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
-
 }
--- src/test/java/com/example/Team1webshop/resources/features/homePage.feature
+++
@@ -1,8 +0,0 @@
-Feature: https://webshop-agil-testautomatiserare.netlify.app/
-  This feature includes tests to verify functionality of the start page
-
-  Background: The user is on the start page
-    Given User has navigated to the start page
-
-  Scenario: page title test
-    Then the page title should be Webbutiken
