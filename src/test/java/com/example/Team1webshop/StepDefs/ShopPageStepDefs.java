2abbd6664ddb2d0de65cdbdbf7a610cb0762a53a
dkakandki <kakandkidivya@gmail.com>
2024-03-23 12:55:37 +0100

    Adding all testcases for filter option,total 5

--- src/test/java/com/example/Team1webshop/StepDefs/ShopPageStepDefs.java
+++ src/test/java/com/example/Team1webshop/StepDefs/ShopPageStepDefs.java
@@ -20,11 +20,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ShopPageStepDefs {
     static WebDriver driver;
 
-    @Given("User has navigated to the shop page") //divya
-    public void setup(){
+    @Given("The user clicks on the shop page") //divya
+    public void setup() throws InterruptedException {
         driver = new ChromeDriver();
         driver.get("https://webshop-agil-testautomatiserare.netlify.app/");
         driver.manage().window().maximize();
+        WebElement shopButton = driver.findElement(By.xpath("/html//header//ul//a[@href='/products']"));
+        shopButton.click();
+        Thread.sleep(2000);
+    }
+
+    @Then("The user is navigated to the shop page") //divya
+    public void I_should_be_navigated_to_Shop_page() {
+        String navigated_url = driver.getCurrentUrl();
+        Assertions.assertEquals("https://webshop-agil-testautomatiserare.netlify.app/products",navigated_url);
     }
 
 
@@ -35,24 +44,8 @@ public class ShopPageStepDefs {
         }
     }
 
-    @When("I click on the shop Button from Webbutiken") //divya
-    public void I_click_on_the_shop_Button_from_Webbutiken() {
-        WebElement shopButton = driver.findElement(By.xpath("/html//header//ul//a[@href='/products']"));
-        shopButton.click();
-    }
-    @Then("I should be navigated to Shop page") //divya
-    public void I_should_be_navigated_to_Shop_page() {
-        String navigated_url = driver.getCurrentUrl();
-        Assertions.assertEquals("https://webshop-agil-testautomatiserare.netlify.app/products",navigated_url);
-    }
-
-
-
     @When("I click on the mens clothing from shop page") //divya
-    public void i_click_on_the_mens_clothing_from_shop_page() throws InterruptedException {
-        WebElement shopButton = driver.findElement(By.xpath("/html//header//ul//a[@href='/products']"));
-        shopButton.click();
-        Thread.sleep(2000);
+    public void i_click_on_the_mens_clothing_from_shop_page() {
         WebElement filterMensClothing = driver.findElement(By.partialLinkText("Men's clothing"));
         filterMensClothing.click();
     }
@@ -64,6 +57,62 @@ public class ShopPageStepDefs {
         List<WebElement> mens_product = mens_clothing.findElements(By.className("card-body"));
         int numberOfElements = mens_product.size();
         Assertions.assertEquals(4, numberOfElements);
+    }
+
+    @When("I click on the womens clothing from shop page")
+    public void i_click_on_the_womens_clothing_from_shop_page() {
+        WebElement filterWomensClothing = driver.findElement(By.partialLinkText("Women's clothing"));
+        filterWomensClothing.click();
+    }
+    @Then("Only womens clothing products must be visible")
+    public void only_womens_clothing_products_must_be_visible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement mens_clothing = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main")));
+        List<WebElement> mens_product = mens_clothing.findElements(By.className("card-body"));
+        int numberOfElements = mens_product.size();
+        Assertions.assertEquals(6, numberOfElements);
+    }
+
+    @When("I click on the jewelery from shop page")
+    public void i_click_on_the_jewelery_from_shop_page() {
+        WebElement filterjewelery = driver.findElement(By.partialLinkText("Jewelery"));
+        filterjewelery.click();
+    }
+    @Then("Only jewelery products must be visible")
+    public void only_jewelery_products_must_be_visible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement mens_clothing = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main")));
+        List<WebElement> mens_product = mens_clothing.findElements(By.className("card-body"));
+        int numberOfElements = mens_product.size();
+        Assertions.assertEquals(4, numberOfElements);
+    }
+
+    @When("I click on the electronics from shop page")
+    public void i_click_on_the_electronics_from_shop_page() {
+        WebElement filterelectronics = driver.findElement(By.partialLinkText("Electronics"));
+        filterelectronics.click();
+    }
+    @Then("Only electronic products must be visible")
+    public void only_electronic_products_must_be_visible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement mens_clothing = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main")));
+        List<WebElement> mens_product = mens_clothing.findElements(By.className("card-body"));
+        int numberOfElements = mens_product.size();
+        Assertions.assertEquals(6, numberOfElements);
+    }
+
+    @When("I click on the all from shop page")
+    public void i_click_on_the_all_from_shop_page() {
+        WebElement filterelectronics = driver.findElement(By.partialLinkText("All"));
+        filterelectronics.click();
+    }
+    @Then("Only all products must be visible")
+    public void only_all_products_must_be_visible() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement mens_clothing = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("main")));
+        List<WebElement> mens_product = mens_clothing.findElements(By.className("card-body"));
+        int numberOfElements = mens_product.size();
+        Assertions.assertEquals(20, numberOfElements);
     }
 
 }
--- src/test/java/com/example/Team1webshop/resources/features/shopPage.feature
+++ src/test/java/com/example/Team1webshop/resources/features/shopPage.feature
@@ -1,14 +1,25 @@
 Feature: Test cases for shop page
 
   Background: The user is on the shop page
-    Given User has navigated to the shop page
-
-  Scenario:I click on Shop button
-    When I click on the shop Button from Webbutiken
-    Then I should be navigated to Shop page
+    Given The user clicks on the shop page
+    Then The user is navigated to the shop page
 
   Scenario:I filter products by mens clothing
     When I click on the mens clothing from shop page
     Then Only mens clothing products must be visible
 
+  Scenario:I filter products by womens clothing
+    When I click on the womens clothing from shop page
+    Then Only womens clothing products must be visible
 
+  Scenario:I filter products by jewelery
+    When I click on the jewelery from shop page
+    Then Only jewelery products must be visible
+
+  Scenario:I filter products by electronics
+    When I click on the electronics from shop page
+    Then Only electronic products must be visible
+
+  Scenario:I remove the filter
+    When I click on the all from shop page
+    Then Only all products must be visible
